5696d7b46a3d97104b6f7fdab0c1fbfb72c6f8f8
Jeovan Delavechea <jeovan.delavechea88@gmail.com>
2022-12-15 21:45:19 -0300

    codigo js adicionado

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+const cartoes = document.querySelectorAll('.cartao');
+const setaAvancar = document.getElementById('btn-avancar')
+const setaVoltar = document.getElementById('btn-voltar')
+let cartaoAtual = 0;
+
+function esconderCartoes() {cartoes.forEach(cartao =>{
+    cartao.classList.remove('selecionado')})}
+function mostrarCartoes() {cartoes[cartaoAtual].classList.add('selecionado')}
+
+setaAvancar.addEventListener('click', function(){
+    const totalDeCartoes = cartoes.length - 1;
+    if(cartaoAtual === totalDeCartoes) {return;}
+    esconderCartoes()
+    cartaoAtual++
+    mostrarCartoes()
+})
+
+setaVoltar.addEventListener('click', function(){
+    if(cartaoAtual === 0) return;
+    esconderCartoes()
+    cartaoAtual--
+    mostrarCartoes()
+})
+
+// function mostrarOuEsconderSetas(){
+//     const chegouNoUltimoCartao = cartaoAtual === cartoes.length - 1;
+//     if(chegouNoUltimoCartao){
+//         set
+//     }
+// }
